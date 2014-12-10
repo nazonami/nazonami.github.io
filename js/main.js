61dfad1267bd1f818eeeb3ddde44f7dbdd6418b5
nazonami <nazona.minami@gmail.com>
2014-12-10 23:18:10 +0900

    ver 0.12

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ var SCREEN_WIDTH    = 320;  // スクリーンの幅
 var SCREEN_HEIGHT   = 480;  // スクリーンの高さ
 var LIMIT_TIME = 20; // 制限時間
 
-var COMBO = 0;
+var combo = 0;
 
 window.onload = function() {
     var game = new Core(SCREEN_WIDTH, SCREEN_HEIGHT);
@@ -17,7 +17,7 @@ window.onload = function() {
     game.preload('./img/mascot_chara.png', './img/all.png', './img/logo.png', './img/button.png','./img/hacka_chan.png'); 
 
     var ButtonImage = enchant.Class.create(enchant.Sprite, {
-        initialize: function(x, y, text) {
+        initialize: function(x, y) {
             enchant.Sprite.call(this, 233, 53);
             this.x = x - 116;
             this.y = y - 12;
@@ -27,22 +27,21 @@ window.onload = function() {
     });
 
     var Chara = enchant.Class.create(enchant.Sprite, {
-        initialize: function(x, y) {
+        initialize: function(x, y, f) {
             enchant.Sprite.call(this, 118, 256);
             this.x = x;
             this.y = y;
             this.image = game.assets['./img/mascot_chara.png'];
-            this.frame = 0;
+            this.frame = f % 6;
         }
     });
 
     var TargetChara = enchant.Class.create(Chara, {
         initialize: function() {
-            Chara.call(this, 0, rand(SCREEN_HEIGHT - 200));
-            this.frame = rand(5);
-            this.scaleX = 0.85* plusOrMinus(); 
-            this.scaleY = 0.85
-            this.speed = (rand(24) + 1) * plusOrMinus();
+            Chara.call(this, 0, rand(SCREEN_HEIGHT - 200), rand(5));
+            this.scaleX = 0.8 * plusOrMinus(); 
+            this.scaleY = 0.8
+            this.speed = (this.frame + 1) * 2.5 * plusOrMinus();
             if (this.speed < 0){
                 this.x = SCREEN_WIDTH;
             }
@@ -55,9 +54,8 @@ window.onload = function() {
             });
             // タッチイベントを設定する
             this.addEventListener('touchstart', function(e) {
-                COMBO ++;
-                game.score += COMBO * Math.abs(this.speed);
-                var comboCount = new Label(COMBO + 'HIT!');
+                combo ++;
+                var comboCount = new Label(combo + 'HIT!');
                 comboCount.textAlign = 'center';
                 comboCount.x = this.x - SCREEN_WIDTH/2;
                 comboCount.y = this.y + 10;
@@ -66,7 +64,8 @@ window.onload = function() {
                 this.parentNode.addChild(comboCount);
                 comboCount.tl.moveBy(0, -10, 8).then(function(){
                     this.parentNode.removeChild(comboCount);
-                });
+                });              
+                game.score += 100 * (this.frame + 1);
                 this.tl.moveTo(this.x, -256, 6).and().rotateBy(-360, 8);
             });
         }
@@ -93,9 +92,10 @@ window.onload = function() {
             scene.addChild(logo); 
             // タイトルラベル設定
             var title = new Label('マスコットすまっしゅ！');
-            title.textAlign = 'center';    
             title.color = '#494949'; 
-            title.x = 12; 
+            title.textAlign = 'center';
+            title.width = SCREEN_WIDTH;
+            title.x = 0; 
             title.y = 24;     
             title.font = '24px sans-serif';    
             scene.addChild(title);    
@@ -105,7 +105,8 @@ window.onload = function() {
             var specialThanksGroup = new Group();
             var  start = new Label('はじめる');  
              start.textAlign = 'center';         
-             start.x = 12;              
+             start.width = SCREEN_WIDTH;
+             start.x = 0;              
              start.y = 320;           
              start.font = '28px sans-serif';     
             var startButton = new ButtonImage(SCREEN_WIDTH/2, 320);
@@ -114,8 +115,9 @@ window.onload = function() {
             scene.addChild(startGroup);
 
             var specialThanks = new Label('すぺしゃる さんくす');  
-            specialThanks.textAlign = 'center';         
-            specialThanks.x = 12;              
+            specialThanks.textAlign = 'center';
+            specialThanks.width = SCREEN_WIDTH;
+            specialThanks.x = 0;              
             specialThanks.y = 406;           
             specialThanks.font = '20px sans-serif';
             var specialThanksButton = new ButtonImage(SCREEN_WIDTH/2, 400);
@@ -128,7 +130,7 @@ window.onload = function() {
             startGroup.addEventListener('touchstart', function(e) {
                 scene.removeChild(startImage);
                 scene.removeChild(logo);
-                game.replaceScene(createGameScene());    
+                game.replaceScene(createRuleScene());    
             });
             specialThanksGroup.addEventListener('touchstart', function(e) {
                 scene.removeChild(startImage);
@@ -150,11 +152,56 @@ window.onload = function() {
             license._element = document.createElement('div');
             license.width = 320;
             license.height = 480; 
-            license._element.innerHTML = '<h1>お借りしたキャラクター</h1><p>プロ生ちゃん（暮井 慧）</br>© 2011 プログラミング生放送</p><p>あんずちゃん（美雲あんず）</br>©GMO Internet, Inc.   ※再使用は禁止です</p><p>美雲このは</br>©GMO Internet, Inc.  ※再使用は禁止です</p><p>クラウディア・窓辺 (Claudia Madobe)</br>© 2011 Microsoft Corporation All Rights Reserved.</p><p>ユニティちゃん（大鳥こはく）</p><img src="http://unity-chan.com/images/imageLicenseLogo.png"  width="60" height="60" alt="ユニティちゃんライセンス"><p>このコンテンツは、『ユニティちゃんライセンス』で提供されています</p><p>クエリ・ラブクラフト（クエリちゃん）</p><p>ハッカドール１号</p>';
+            license._element.innerHTML = '<div style="font-size:8pt;"><h1>お借りしたキャラクター</h1><p>プロ生ちゃん（暮井 慧）</br>© 2011 プログラミング生放送</p><p>あんずちゃん（美雲あんず）</br>©GMO Internet, Inc.   ※再使用は禁止です</p><p>美雲このは</br>©GMO Internet, Inc.  ※再使用は禁止です</p><p>クラウディア・窓辺 (Claudia Madobe)</br>© 2011 Microsoft Corporation All Rights Reserved.</p><p>ユニティちゃん（大鳥こはく）</p><img src="http://unity-chan.com/images/imageLicenseLogo.png"  width="30" height="30" alt="ユニティちゃんライセンス"><p>このコンテンツは、『ユニティちゃんライセンス』で提供されています</p><p>クエリ・ラブクラフト（クエリちゃん）</p><p>ハッカドール１号</p></div>';
             scene.addChild(license);
             scene.addEventListener('touchstart', function(e) {
                 game.replaceScene(createStartScene());    
             });
+            return scene;
+        };
+
+        /**
+        * ルール説明シーン
+        */
+        var createRuleScene = function() {
+             var scene = new Scene(); 
+            scene.backgroundColor = 'rgba(0, 0, 0, 0)'; 
+            var rule = new Entity();
+            rule._element = document.createElement('div');
+            rule.width = 320;
+            rule.height = 480; 
+            rule._element.innerHTML = '<h1>  ルール説明 </h1><p>  流れてくるマスコットをクリックしますっ！  </p><p>  右のキャラほど得点が多くもらえちゃいますっ！  </p>';
+            scene.addChild(rule);
+            scene.addEventListener('touchstart', function(e) {
+                game.replaceScene(createGameScene());    
+            });
+            // キャラの初期表示
+            for(var i = 0; i < 6; i++){
+                var mascot = new Chara(i * 35, 48, i);
+                mascot.scaleX = 0.6; 
+                mascot.scaleY = 0.6;
+                scene.addChild(mascot);
+            }
+
+
+            // ハッカドールちゃん
+            var sprite = new Sprite(256, 256);
+            sprite.x = 36 ;
+            sprite.y = SCREEN_HEIGHT - 256;
+       
+            sprite.image = game.assets['./img/hacka_chan.png'];
+            scene.addChild(sprite);
+            // アニメ
+            sprite.addEventListener('enterframe', function() {            
+                sprite.frame = [0, 2, 1, 2, 3, 2][Math.floor(sprite.age/12) % 6] + 10;
+            });   
+
+            var label = new Label('画面をタッチ!');
+            label.font = '24px sans-serif';  
+            label.x = 0; 
+            label.y = 420; 
+            scene.addChild(label); 
+
             return scene;
         };
         /**
@@ -163,9 +210,9 @@ window.onload = function() {
         var createGameScene = function() {
             var scene = new Scene();  
             scene.backgroundColor = '#ffffff';
-            
+          
             var time =  LIMIT_TIME;
-            var COMBO = 0;
+            combo = 0;
             game.score = 0;
             // 得点欄を作成
             var label = new Label(''); 
@@ -193,6 +240,7 @@ window.onload = function() {
             }
 
             scene.addEventListener('enterframe', function(){
+
                 // 残り時間の表示を更新
                 if(this.age % game.fps == 0){
                     time --; 
@@ -202,22 +250,23 @@ window.onload = function() {
                         game.replaceScene(createGameoverScene());
                     }
                 }
-                label.text = 'スコア: ' + game.score + '点';
-
                 // キャラクター生成
-                if(this.age % 5 == 0){
+                if(this.age % 6 == 0){
                     var mascot = new TargetChara();
                     scene.addChild(mascot);
+                    label.text = 'スコア: ' + game.score + '点';
                 }
 
                 // ハッカドールちゃんアニメ
-                sprite.frame = [0, 1, 2, 3][Math.floor(sprite.age/24) % 4] + 10;
+                sprite.frame = [0, 1, 2, 3, 2, 1][Math.floor(sprite.age/8) % 6];
             })
 
 
             // ゲームシーンを返す
             return scene;
         };
+
+
         /**
         * ゲームオーバーシーン
         */
@@ -226,7 +275,7 @@ window.onload = function() {
             scene.backgroundColor = 'rgba(0, 0, 0)';  
 
             // スコアラベル設定
-            var comboCount = new Label(COMBO + 'HIT!');
+            var comboCount = new Label(combo + 'HIT!');
             comboCount.textAlign = 'center';
             comboCount.x = 0;
             comboCount.y = 20;
